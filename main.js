24c4e234c61c22a8184a983ccefcddcc5a73ce4c
Sania Kiran <153180171+trioclab@users.noreply.github.com>
2024-02-06 13:48:00 +0500

    updated

--- main.js
+++ main.js
@@ -1,41 +1,53 @@
 // generating random number using Math.random and assign it in the variable randomNumber
-let randomNumer = Math.random();
+let randomNumber = Math.random();
 
 // function if user click rock button
-function rock(){
-    randomNumer = Math.random();
-    if (randomNumer >= 0 && randomNumer < 1/3){
-    alert('tie');}
-    else if (randomNumer >= 1/3 && randomNumer < 2/3){
-    alert('computer win!');}
-    else if( randomNumer >= 2/3 && randomNumer < 1){
-    alert('you win!');}
-    else{
-    alert('error');}
+function rock() {
+  randomNumber = Math.random();
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    randomNumber = "rock";
+    alert(`computer choose ${randomNumber}, Game is tie.`);
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    randomNumber = "scissor";
+    alert(`computer choose ${randomNumber} ,you win!`);
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    randomNumber = "paper";
+    alert(`computer choose ${randomNumber} ,computer win!`);
+  } else {
+    alert("error");
+  }
 }
 
 // function if user click paper button
-function paper(){
-    randomNumer = Math.random();
-    if (randomNumer >= 0 && randomNumer < 1/3){
-    alert('you win!');}
-    else if (randomNumer >= 1/3 && randomNumer < 2/3){
-    alert('tie');}
-    else if( randomNumer >= 2/3 && randomNumer < 1){
-        alert('computer win!');}
-    else{
-        alert('error');}
-    }
+function paper() {
+  randomNumber = Math.random();
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    randomNumber = "rock";
+    alert(`computer choose ${randomNumber} , you win!`);
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    randomNumber = "scissor";
+    alert(`computer choose ${randomNumber} , computer win !`);
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    randomNumber = "paper";
+    alert(`computer choose ${randomNumber} , tie`);
+  } else {
+    alert("error");
+  }
+}
 
 // function if user click scissor button
-    function scissors(){
-    randomNumer = Math.random();
-    if (randomNumer >= 0 && randomNumer < 1/3){
-    alert('computer win!');}
-    else if (randomNumer >= 1/3 && randomNumer < 2/3){
-    alert('you win');}
-    else if( randomNumer >= 2/3 && randomNumer < 1){
-    alert('tie!');}
-    else{
-    alert('error');}
-} 
+function scissors() {
+  randomNumber = Math.random();
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    randomNumber = "rock";
+    alert(`computer choose ${randomNumber}, computer win!`);
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    randomNumber = "scissor";
+    alert(`computer choose ${randomNumber}, tie `);
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    randomNumber = "paper";
+    alert(`computer choose ${randomNumber} , you win!`);
+  } else {
+    alert("error");
+  }
+}
